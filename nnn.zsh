4ae1c1f2fe419da21a75e0346dac111595d9deae
Griffen Wakelin <gwakelin@upei.ca>
2023-03-12 12:41:25 -0300

    added nnn.zsh and changed tmux configs

---
+++ nnn.zsh
@@ -0,0 +1,9 @@
+# NNN
+
+
+# TURN ON PLUGINS FOR NNN
+
+export NNN_PLUG="o:fzp-open;p:preview-tui"
+export NNN_FCOLORS="0000E6310000000000000000"
+export NNN_FIFO=/tmp/nnn.fifo
+export NNN_OPTS="He"
