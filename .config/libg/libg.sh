726d99dc263fddab558d300791afa9f78ff8b987
Griffen Wakelin <wakelin@dal.ca>
2024-07-03 22:41:50 -0300

    Removed many useless dirs

--- .config/libg/libg.sh
+++
@@ -1,4 +0,0 @@
-DEFAULT_RESPERPAGE=100 # default number of search results per page (allowed values: 25, 50, 100)	
-DEFAULT_DEPTH=1 # default number of result pages to scan (allowed values: any positive integer)	
-LIBGEN_MIRROR="https://libgen.rs" # alternatives: *.is, *.rs, *.st	
-DOWNLOAD_LOCATION="" # default download location (if empty then books will be downloaded to current directory)
